6892cfa692eefffc64e7822e38e132e3e6492bb1
Marcel Sicking <marcel.sicking01@vodafone.com>
2022-06-24 16:17:03 +0200

    corrections in response authenticated

--- src/main/java/com/vodafone/foodbox/controller/UserController.java
+++ src/main/java/com/vodafone/foodbox/controller/UserController.java
@@ -19,22 +19,43 @@ public class UserController {
 	
 	
 	private class AuthenticationInfo{
-		String email="";
-		boolean authenticated=false;
-		String message="Failed to authenticate";
+		private String email="";
+		private boolean authenticated=false;
+		private String message="Failed to authenticate";
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public boolean isAuthenticated() {
+			return authenticated;
+		}
+		public void setAuthenticated(boolean authenticated) {
+			this.authenticated = authenticated;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
 	}
-	@PostMapping("/isAuthenticated")
+	@PostMapping("/authenticate")
 	public ResponseEntity<AuthenticationInfo> authenticate(@RequestBody LoginUser userToLogin) throws Exception{
 		LoginUser loginUser = loginUserService.getUserByEmail(userToLogin.getEmail());
 		AuthenticationInfo authenticationInfo = new AuthenticationInfo();
 		if (loginUser==null) {
 			System.out.println("user not found in db");
+			authenticationInfo.setAuthenticated(false);
 		} else if (!loginUser.getPassword().equals(userToLogin.getPassword())) {
 			System.out.println("incorrect pw");
+			authenticationInfo.setAuthenticated(false);
+		} else if (loginUser.getPassword().equals(userToLogin.getPassword())) {
+			authenticationInfo.setAuthenticated(true);
+			authenticationInfo.setEmail(userToLogin.getEmail());
+			authenticationInfo.setMessage("Welcome "+userToLogin.getEmail());
 		}
-		authenticationInfo.authenticated=true;
-		authenticationInfo.email=userToLogin.getEmail();
-		authenticationInfo.message="Welcome "+userToLogin.getEmail();
 		return ResponseEntity.ok(authenticationInfo);
 	}
 	
